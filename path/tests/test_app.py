7ebe823eda0eb5b9aef614a52b5c34a466065293
heavenboy8 <sebastienlambert.lambert@laposte.net>
2023-02-17 15:33:38 +0100

    :bug: fix tests when using a not self-signed cert

--- path/tests/test_app.py
+++ path/tests/test_app.py
@@ -2,23 +2,35 @@ import requests
 
 
 def test_read_write(url, certificate):
-    response = requests.get(url, verify=certificate)
+    response = requests.get(
+        url,
+        headers={
+            "Authorization": "Bearer bAyJhel6vwzrvNcy7ux2nULRwpP6BviE34KSiZRGixo="
+        },
+        verify=certificate,
+    )
     assert response.status_code == 404
     assert b"You should write before read" in response.content
 
-    response = requests.post(url, verify=certificate)
+    response = requests.post(
+        url,
+        headers={
+            "Authorization": "Bearer 6fMvPktkMwZj5UJwxasOIj7sO37H4DfZZo05Nn1fFYw="
+        },
+        verify=certificate,
+    )
     assert response.status_code == 200
 
-    response = requests.get(url, verify=certificate)
+    response = requests.get(
+        url,
+        headers={
+            "Authorization": "Bearer bAyJhel6vwzrvNcy7ux2nULRwpP6BviE34KSiZRGixo="
+        },
+        verify=certificate,
+    )
     assert response.status_code == 200
 
 
 def test_health(url, certificate):
     response = requests.get(f"{url}/health", verify=certificate)
     assert response.status_code == 200
-
-
-def test_login(url, certificate):
-    response = requests.get(f"{url}/whoami", verify=certificate)
-    assert response.status_code == 200
-    assert b"username" in response.content
--- yaos_millionaires/tests/test_app.py
+++ yaos_millionaires/tests/test_app.py
@@ -2,40 +2,40 @@ import requests
 
 
 def test_average(url, certificate):
-    response = requests.delete(f'{url}/', verify=certificate)
+    response = requests.delete(f"{url}/", verify=certificate)
     assert response.status_code == 200
 
-    response = requests.get(f'{url}/', verify=certificate)
+    response = requests.get(f"{url}/", verify=certificate)
     assert response.status_code == 200
     assert response.json() == {"max": None}
 
-    response = requests.post(f'{url}/', json={"n": 100.0}, verify=certificate)
+    response = requests.post(f"{url}/", json={"n": 100.0}, verify=certificate)
     assert response.status_code == 200
 
-    response = requests.get(f'{url}/', verify=certificate)
+    response = requests.get(f"{url}/", verify=certificate)
     assert response.status_code == 200
-    assert response.json() == {"max": 'User #1'}
+    assert response.json() == {"max": "User #1"}
 
-    response = requests.post(f'{url}/', json={"n": 200.0}, verify=certificate)
-    assert response.status_code == 200
-
-    response = requests.get(f'{url}/', verify=certificate)
-    assert response.status_code == 200
-    assert response.json() == {"max": 'User #2'}
-
-    response = requests.delete(f'{url}/', verify=certificate)
+    response = requests.post(f"{url}/", json={"n": 200.0}, verify=certificate)
     assert response.status_code == 200
 
-    response = requests.get(f'{url}/', verify=certificate)
+    response = requests.get(f"{url}/", verify=certificate)
+    assert response.status_code == 200
+    assert response.json() == {"max": "User #2"}
+
+    response = requests.delete(f"{url}/", verify=certificate)
+    assert response.status_code == 200
+
+    response = requests.get(f"{url}/", verify=certificate)
     assert response.status_code == 200
     assert response.json() == {"max": None}
 
 
 def test_average_error(url, certificate):
-    response = requests.post(f'{url}/', verify=certificate)
+    response = requests.post(f"{url}/", verify=certificate)
     assert response.status_code == 422
 
-    response = requests.post(f'{url}/', json={"n": "str"}, verify=certificate)
+    response = requests.post(f"{url}/", json={"n": "str"}, verify=certificate)
     assert response.status_code == 422
 
 
